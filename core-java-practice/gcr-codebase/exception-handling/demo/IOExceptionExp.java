fb7182f6945daf0ecea2194603d6ed2ef0317f6c
Sneha-6392 <sneha.mauryasm02@gmail.com>
2026-06-17 12:22:34 +0530

    exception practice

--- core-java-practice/gcr-codebase/exception-handling/demo/IOExceptionExp.java
+++ core-java-practice/gcr-codebase/exception-handling/demo/IOExceptionExp.java
@@ -2,14 +2,12 @@ import java.io.*;
 
 public class IOExceptionExp {
     public static void main(String[] args) {
-        try {
-            FileReader fr = new FileReader("data.txt");
+        try (FileReader fr = new FileReader("data.txt")) {
             int ch;
             while ((ch = fr.read()) != -1) {
                 System.out.println((char) ch);
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
